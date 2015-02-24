896d87ffab4424d7181e2d715c1dda5c24035cb9
Irene <irenerong1012@gmail.com>
2015-02-24 17:20:57 +0100

    ireneajoutedeuxfichier

---
+++ EASE-server/api/controllers/SortWFController.js
@@ -0,0 +1,11 @@
+/**
+ * SortWFController
+ *
+ * @description :: Server-side logic for managing sortwfs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
